d55ea49e4b23ccd030bbd881ce9fe978ec55ef91
arthurpiratas <silva.arthursoares@gmail.com>
2021-06-27 16:08:39 -0300

    modificando retorno Json

--- src/servico/consultaEventosArtista.py
+++ src/servico/consultaEventosArtista.py
@@ -20,7 +20,7 @@ def listaEventosArtistas(id_Artista):
         for (i,linhas) in EventosMusico.head(10 if linhas[0] >= 10 else linhas[0]).iterrows():
             objetoID = {'ID_Evento' :  str(EventosMusico.at[i,'ID_Evento']), 'Nome_Organizador' : str(EventosMusico.at[i,'Nome_Organizador']), 'nota_avaliacao_organizador' : str(EventosMusico.at[i,'nota_avaliacao_organizador']), 'Local_Evento' : str(EventosMusico.at[i,'Local_Evento']), 'nomeEvento' : str(EventosMusico.at[i,'nomeEvento']), 'Descrição_Evento' : str(EventosMusico.at[i,'Descrição_Evento']), 'data' : str(EventosMusico.at[i,'data']), 'horario' : str(EventosMusico.at[i,'horario']), 'status' : str(EventosMusico.at[i,'status']), 'tipoEvento' : str(EventosMusico.at[i,'tipoEvento']), 'duracaoEvento' : str(EventosMusico.at[i,'duracaoEvento']), 'localidade' : EventosMusico.at[i,'localidade'], 'observacoes' : str(EventosMusico.at[i,'observacoes']), 'Valor_total_evento' : str(EventosMusico.at[i,'Valor total evento']), 'latitude' :EventosMusico.at[i,'latitude'], 'longitude' :EventosMusico.at[i,'longitude']}
             ListaEventos.append(objetoID)
-        eventos = {"Eventos" : ListaEventos}
+        eventos = ListaEventos
         return eventos
     
     return retornaLista(linhas)
